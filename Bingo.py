a487d86cea6e418d76c15dae6dc890d7efb87ef4
Jai <jaikamath169@gmail.com>
2025-12-05 21:36:31 +0530

    Improved __repr__

--- Bingo.py
+++ Bingo.py
@@ -6,6 +6,11 @@ class Box:
     def __init__(self):
         self.main_box = self.create_box()
         self.CD = dict.fromkeys(self.main_box, False)
+        self.group_data = {"1-row": False, "2-row": False, "3-row": False, 
+                                 "4-row": False, "5-row": False, "1-col": False, 
+                                 "2-col": False, "3-col": False, "4-col": False, 
+                                 "5-col": False, "1-dia": False, "2-dia": False
+                                 }
     
     def create_box(self):
         B = ["01","02","03","04","05",
@@ -21,14 +26,19 @@ class Box:
         counter = 1
         res = ""
         for i in self.main_box:
-            if counter % 5 == 0 and counter != 1:
+            if counter % 5 == 0:
+                if not self.isCrossed(i):
+                    res+=f"| {i} |"
+                    counter += 1
+                elif self.isCrossed(i):
+                    res += f"| \033[9;31m{i}\033[0m |"
+                    counter += 1
                 res += "\n"
-                counter += 1
             else:
                 if not self.isCrossed(i):
                     res+=f"| {i} |"
                     counter += 1
-                if self.isCrossed(i):
+                elif self.isCrossed(i):
                     res += f"| \033[9;31m{i}\033[0m |"
                     counter += 1
         return res
@@ -41,9 +51,28 @@ class Box:
             return None
         else:
             self.CD[Element] = True
+    
+    def chunk(self, lst, size):
+        return [lst[i:i+size] for i in range(0, len(lst), size)]
+
+    
+    def check_vertical(self):
+        ...
+    
+    def check_horizontal(self):
+        broken_box = self.chunk(self.main_box, 5)
+        counter = 1
+        for i in broken_box:
+            if self.isCrossed(i[0]) and self.isCrossed(i[1]) and self.isCrossed(i[2]) and self.isCrossed(i[3]) and self.isCrossed(i[4]):
+                return f"{counter}-row"
+            else:
+                counter += 1
 
 
 #TESTING:-
 X = Box()
-X.cross_element("20")
-print(X)
+
+while True:
+    print(X)
+    inp = input("Enter:- ")
+    X.cross_element(inp)
